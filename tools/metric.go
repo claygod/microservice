46045a0c9027bcd4bad136e8b9519a866f2f2689
Eduard <claygod@yandex.ru>
2017-01-05 15:22:31 +0300

    Changed work with middleware

--- tools/metric.go
+++ tools/metric.go
@@ -5,40 +5,19 @@ package tools
 // Copyright © 2016 Eduard Sesigin. All rights reserved. Contacts: <claygod@yandex.ru>
 
 import (
-	"context"
 	"net/http"
 	"time"
 
 	"github.com/Sirupsen/logrus"
 )
 
-// NewMetric - create a new Metric
-func NewMetric() *Metric {
-	m := &Metric{logger: logrus.New()}
-	return m
-}
-
-// Metric structure
-// This library shows a simple version of the logging duration metrics
-type Metric struct {
-	logger *logrus.Logger
-}
-
-// Start - fix a starting time
-func (m *Metric) Start(w http.ResponseWriter, req *http.Request) (http.ResponseWriter, *http.Request) {
-	if req != nil {
-		ctx := req.Context()
-		ctx = context.WithValue(ctx, "timeStart", int(time.Now().UnixNano()))
-		req = req.WithContext(ctx)
+func Metric(f func(w http.ResponseWriter, r *http.Request)) func(w http.ResponseWriter, r *http.Request) {
+	logger := logrus.New()
+	return func(w http.ResponseWriter, r *http.Request) {
+		timeStart := int(time.Now().UnixNano())
+		f(w, r)
+		go logger.WithField("duration", int(time.Now().UnixNano())-timeStart).
+			WithField("url", r.URL.Path).
+			Print()
 	}
-	return w, req
-}
-
-// End - sending metrics on the duration
-func (m *Metric) End(w http.ResponseWriter, req *http.Request) (http.ResponseWriter, *http.Request) {
-	if req != nil {
-		timeStart := req.Context().Value("timeStart").(int)
-		go m.logger.WithField("duration", int(time.Now().UnixNano())-timeStart).Print("Demo of metric")
-	}
-	return w, req
 }
